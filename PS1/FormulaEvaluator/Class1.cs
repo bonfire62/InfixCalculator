dec883d6dee152f240a82558841fa8a0827e3ab5
bonfire62 <kenbonar@gmail.com>
2015-09-03 21:18:16 -0600

    Created file, added Namespace, Class, Lookup Delegate, Evaluate Method.

---
+++ PS1/FormulaEvaluator/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaEvaluator
+{
+    public static class Evaluator
+    {
+        public delegate int Lookup(string v);
+
+        public static int Evaluate(string exp, Lookup variableEvaluatorLookup)
+        {
+            Stack<double> valueStack = new Stack<double>();
+            Stack<string> operatorStack = new Stack<string>();
+        }
+
+        
+          
+
+    }
+
+    
+    
+}
