b2462825f5614852ebe6ef720225d6650a743ca4
bonfire62 <kenbonar@gmail.com>
2015-09-08 09:07:12 -0600

    Added parse methods

--- PS1/FormulaEvaluator/Class1.cs
+++ PS1/FormulaEvaluator/Class1.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlTypes;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FormulaEvaluator
@@ -13,15 +17,29 @@ namespace FormulaEvaluator
 
         public static int Evaluate(string exp, Lookup variableEvaluatorLookup)
         {
-            Stack<double> valueStack = new Stack<double>();
+            string[] substrings = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+            for (int i = 0; i < substrings.Length; i++)
+            {
+                substrings[i] = substrings[i].Trim();
+            }
+            Stack<int> valueStack = new Stack<int>();
             Stack<string> operatorStack = new Stack<string>();
+
+            foreach (var i in substrings)
+            {
+                int k;
+                if (int.TryParse(i, out k))
+                {
+                    valueStack.Push(k);
+                }
+                operatorStack.Push(i);
+            }           
+            
+            
         }
 
-        
-          
-
-    }
-
     
-    
+
+
+
 }
